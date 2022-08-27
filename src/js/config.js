92c3e7d655cbdb71f9f40ea606250fc8c5f75b8a
TakaoKishimoto <takao.k.04@gmail.com>
2022-08-26 19:23:22 -0700

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SET = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4eb0e3c1-54ec-4369-bc4e-00139ae6698f';
+export const MODAL_CLOSE_SEC = 2.5;
